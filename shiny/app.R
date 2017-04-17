381e67932161e4649583a6752882bca56c7e00d8
amycrawford <amycrawford@Amys-MacBook-Pro.local>
2017-04-17 16:00:48 -0500

    na omit

--- shiny/app.R
+++ shiny/app.R
@@ -4,9 +4,12 @@ require(dplyr)
 library(plotly)
 library(rvest)
 
-#load("~/Desktop/IowaState/Semester4/Stat585X/Project/sun-shiny-weather/data/temps.Rda")
+#load("../data/temps.Rda")
 ##### I'm not sure why this did this... it is in the data folder so we should just be able to look at it
 # Define UI for application that draws a histogram
+temps <- temps %>% na.omit()
+
+
 shinyApp(
   ui = navbarPage(
     "NYT Weather Chart: \n Major Midwestern Cities",
